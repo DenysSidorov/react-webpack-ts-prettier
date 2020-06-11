98c021da7e7db547f884a52cfbf9fe833c97bb59
Denys Sidorov <000scorpions000@gmail.com>
2020-06-11 07:53:37 +0300

    update ts/babel

--- src/app.tsx
+++ src/app.tsx
@@ -1,8 +1,12 @@
 import React from 'react';
 import ReactDom from 'react-dom';
+// import './types';
+// import './interf';
+import './enum';
 
 const App = () => {
     let a: string = '8';
+    a = '4';
     return (<p>Variable from ts syntax - {a}</p>);
 }
 
