898b6624a5381c0011b947b1615e29c992a07110
Denys Sidorov <000scorpions000@gmail.com>
2020-07-29 22:41:42 +0300

    add examples

--- src/app.tsx
+++ src/app.tsx
@@ -1,12 +1,12 @@
 import React from 'react';
 import ReactDom from 'react-dom';
-// import './types';
-// import './interf';
-import './enum';
+import './clean_ts_examples/3_enum';
 
 const App = () => {
     let a: string = '8';
     a = '4';
+
+
     return (<p>Variable from ts syntax - {a}</p>);
 }
 
